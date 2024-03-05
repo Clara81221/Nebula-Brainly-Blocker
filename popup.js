5c7fec9a712bd6a2fc70262cad0dc0be364de092
Clara <62320586+Clara81221@users.noreply.github.com>
2024-03-05 16:22:58 -0500

    Delete popup.js

--- popup.js
+++
@@ -1,6 +0,0 @@
-// popup.js
-document.getElementById('deleteButton').addEventListener('click', () => {
-    // Execute the content script to delete specific value from local storage
-    browser.tabs.executeScript({file: "content.js"});
-  });
-  
